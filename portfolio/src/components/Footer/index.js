80b2f5c54501b0d093e875a3eed08bc9122e3792
SV <72838881+santis1001@users.noreply.github.com>
2023-08-01 13:40:37 -0600

    fixed footer

--- portfolio/src/components/Footer/index.js
+++ portfolio/src/components/Footer/index.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 
+import git from "../../assets/images/github-mark-white.png";
+import msg from "../../assets/images/Mail.png";
+import iN from "../../assets/images/In-White-40@2x.png";
+
 const Footer = () => {
     return (
         <footer>
@@ -7,16 +11,19 @@ const Footer = () => {
                 <h2>Contact</h2>
                 <ul class="contact">
                     <li>
-                        <a href="tel:+526311021568">Phone: +52-631-102-1568</a>
+                        <a href="mailto:santis1001@gmail.com">
+                            <img src={msg} alt='mail' />
+                        </a>
                     </li>
                     <li>
-                        <a href="mailto:santis1001@gmail.com">Email</a>
+                        <a href="https://github.com/santis1001">
+                            <img src={git} alt='git' />
+                        </a>
                     </li>
                     <li>
-                        <a href="https://github.com/santis1001">Github</a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/santiago-valenzuela-/">LinkedIn</a>
+                        <a href="https://www.linkedin.com/in/santiago-valenzuela-/">
+                            <img src={iN} alt='in' />
+                        </a>
                     </li>
                 </ul>
             </div>
