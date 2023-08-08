44d3c9045ee49fb94a13be61e4ee6e10a2166fd1
SV <72838881+santis1001@users.noreply.github.com>
2023-08-07 20:33:48 -0600

    adding git link to portfolio

--- portfolio/src/components/Project/portfolio/index.js
+++ portfolio/src/components/Project/portfolio/index.js
@@ -3,8 +3,9 @@ import React, { useState } from 'react';
 import p2 from '../../../assets/images/project-app.JPG'
 import vac from '../../../assets/images/vaccine_sim.JPG'
 import bus from '../../../assets/images/Bus_app.JPG'
-import msg from '../../../assets/images/And_Dev.png'
+import ss from '../../../assets/images/solarproject.png'
 import id from '../../../assets/images/solidworks.PNG'
+import git from "../../../assets/images/github-mark-white.png";
 
 const Portfolio = () => {
 
@@ -17,8 +18,8 @@ const Portfolio = () => {
     const busimg = {
         backgroundImage: `url(${bus})`
     };
-    const msgimg = {
-        backgroundImage: `url(${msg})`
+    const ssimg = {
+        backgroundImage: `url(${ss})`
     };
     const idimg = {
         backgroundImage: `url(${id})`
@@ -31,28 +32,30 @@ const Portfolio = () => {
                     <h2 id="Work">Work</h2>
                 </div>
                 <div class="container_work scrollbar-morpheus-den">
-
-                    <div class="workbanner">
-                        <div class="workbannerback">
-                            <a href="https://github.com/santis1001/TrackYourGoals">
+                    <ul class="grid">
+                        <li>
+                            <a href="https://track-your-goals-180d59665ffa.herokuapp.com/login">
                                 <div class="cards_work" id="project2">
                                     <div class="transparent_mask_work"></div>
                                     <div class="card_text_work">
                                         <h3>Track Finances Web App</h3>
+                                        <a href="https://github.com/santis1001/TrackYourGoals">
+                                            <img src={git} alt='git' />
+                                        </a>
                                     </div>
                                     <div class='img' style={p2img} ></div>
                                 </div>
                             </a>
-                        </div>
-                    </div>
-
-                    <ul class="grid">
+                        </li>
                         <li>
                             <a href="./assets/pdf/Vaccine_sim.pdf">
                                 <div class="cards_work" id="vacc">
                                     <div class="transparent_mask_work"></div>
                                     <div class="card_text_work">
                                         <h3>Vaccine Simulation - Java</h3>
+                                        <a href="https://github.com/santis1001">
+                                            <img src={git} alt='git' />
+                                        </a>
                                     </div>
                                     <div class='img' style={vaccimg} ></div>
                                 </div>
@@ -64,6 +67,9 @@ const Portfolio = () => {
                                     <div class="transparent_mask_work"></div>
                                     <div class="card_text_work">
                                         <h3>Bus Routing - Java</h3>
+                                        <a href="https://github.com/santis1001">
+                                            <img src={git} alt='git' />
+                                        </a>
                                     </div>
                                     <div class='img' style={busimg} ></div>
 
@@ -71,13 +77,16 @@ const Portfolio = () => {
                             </a>
                         </li>
                         <li>
-                            <a href="./assets/pdf/AndroidApp.pdf">
-                                <div class="cards_work" id="anddev">
+                            <a href="https://santis1001.github.io/solar-waffle/">
+                                <div class="cards_work" id="solar">
                                     <div class="transparent_mask_work"></div>
                                     <div class="card_text_work">
-                                        <h3>Messaging App - Android Dev</h3>
+                                        <h3>Solar Systen -  Web App</h3>
+                                        <a href="https://github.com/santis1001/solar-waffle">
+                                            <img src={git} alt='git' />
+                                        </a>
                                     </div>
-                                    <div class='img' style={msgimg} ></div>
+                                    <div class='img' style={ssimg} ></div>
                                 </div>
                             </a>
                         </li>
@@ -87,6 +96,23 @@ const Portfolio = () => {
                                     <div class="transparent_mask_work"></div>
                                     <div class="card_text_work">
                                         <h3>Industrial Design</h3>
+                                        <a href="https://github.com/santis1001">
+                                            <img src={git} alt='git' />
+                                        </a>
+                                    </div>
+                                    <div class='img' style={idimg} ></div>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="./assets/pdf/Industrial_Design.PDF">
+                                <div class="cards_work" id="indes">
+                                    <div class="transparent_mask_work"></div>
+                                    <div class="card_text_work">
+                                        <h3>Industrial Design</h3>
+                                        <a href="https://github.com/santis1001">
+                                            <img src={git} alt='git' />
+                                        </a>
                                     </div>
                                     <div class='img' style={idimg} ></div>
                                 </div>
